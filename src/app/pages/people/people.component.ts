3b1ec4329ee3de5addccac20c42f57a8c558a1ef
Alex Tarasenko <alextarase@gmail.com>
2022-07-28 18:11:40 +0700

    final commit

--- src/app/pages/people/people.component.ts
+++ src/app/pages/people/people.component.ts
@@ -8,22 +8,4 @@ import { PlanetsService } from 'src/app/services/planets.service';
   selector: 'app-people',
   templateUrl: './people.component.html',
 })
-export class PeopleComponent {
-  // resident: IPeople[];
-  // planetinf: IPlanet;
-  // residents: IPeople[];
-  // constructor(private peopleService: PeopleService) {}
-  // ngOnInit(): void {
-  //   for (let i = 0; i <= this.planetinf.residents.length - 1; i++) {
-  //     this.peopleService
-  //       .getAll(this.planetinf.residents[i])
-  //       .subscribe((resident) => {
-  //         // this.residents.push(...resident);
-  //         this.resident = resident;
-  //         this.residents.push(...this.resident);
-  //         console.log(...this.resident);
-  //         console.log(...this.residents);
-  //       });
-  //   }
-  // }
-}
+export class PeopleComponent {}
